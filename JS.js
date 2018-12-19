f23c2c1f60223a9df6850a3ee2512ead3ae1b354
Irina Penzina <39900644+IrinaPenzina@users.noreply.github.com>
2018-12-19 15:05:52 +0800

    Update JS.js

--- JS.js
+++ JS.js
@@ -8,28 +8,25 @@ window.onload = function() {
 	bannerLoop();
 	}, bannerTimer);
 	
-	// onmouseover banner stops
 	document.getElementById("main-banner").onmouseover = function() {
 		console.log("Inside onmouseover function")
 		clearInterval(startBannerLoop);
-	}
+	},
 	document.getElementById("main-banner").onmouseout = function() {
-		bannerLoop();
-		console.log("Inside onmouseout function")
 		startBannerLoop = setInterval(function() {
-		bannerLoop();
+	bannerLoop();
 	}, bannerTimer);
 	}
 	document.getElementById("imgbanbtn-prev").onclick = function() {
-		console.log("Inside bannerLoop2 onclick prev function")
+		console.log("Inside bannerLoop2 function")
 		bannerLoop2();
 	}
 	document.getElementById("imgbanbtn-next").onclick = function() {
-		console.log("Inside bannerLoop onclick next function")
 		bannerLoop();
 	}
 }
-	
+
+// first loop to slide pictures to the right
 function bannerLoop() {
 	if (bannerStatus === 1) {
 		console.log("insideBannerLoop")
@@ -105,55 +102,56 @@ function bannerLoop() {
 		bannerStatus = 1;
 	}
 }
-		function bannerLoop2() {
-			if (bannerStatus === 1) {
-				console.log("insideBannerLoop")
-				document.getElementById("travel").style.opacity="0";
-				setTimeout(function() {
-					document.getElementById("work").style.left="0px";
-					document.getElementById("work").style.zIndex="4"; 
-					document.getElementById("travel").style.left="-350px"; 
-					document.getElementById("travel").style.zIndex="3"; 
-					document.getElementById("yoga").style.left="350px"; 
-					document.getElementById("yoga").style.zIndex="2"; 
-					document.getElementById("language").style.left="700px"; 
-					document.getElementById("language").style.zIndex="1"; 
-				}, 500);
+// second loop to slide pictures to the left		
+function bannerLoop2() {
+	if (bannerStatus === 1) {
+		console.log("insideBannerLoop")
+		document.getElementById("travel").style.opacity="0";
+		setTimeout(function() {
+			document.getElementById("work").style.left="0px";
+			document.getElementById("work").style.zIndex="4"; 
+			document.getElementById("travel").style.left="-350px"; 
+			document.getElementById("travel").style.zIndex="3"; 
+			document.getElementById("yoga").style.left="350px"; 
+			document.getElementById("yoga").style.zIndex="2"; 
+			document.getElementById("language").style.left="700px"; 
+			document.getElementById("language").style.zIndex="1"; 
+			}, 500);
 			setTimeout(function() {
-				document.getElementById("travel").style.opacity="1"; 
+			document.getElementById("travel").style.opacity="1"; 
 			}, 1000);
 		
 			bannerStatus = 2;
 			}
-			else if (bannerStatus === 2) {
-				document.getElementById("yoga").style.opacity="0"; 
-				setTimeout(function() {
-					document.getElementById("travel").style.left="0px";
-					document.getElementById("travel").style.zIndex="4"; 
-					document.getElementById("yoga").style.left="-350px"; 
-					document.getElementById("yoga").style.zIndex="3"; 
-					document.getElementById("language").style.left="350px"; 
-					document.getElementById("language").style.zIndex="2"; 
-					document.getElementById("work").style.left="700px"; 
-					document.getElementById("work").style.zIndex="1"; 
+		else if (bannerStatus === 2) {
+			document.getElementById("yoga").style.opacity="0"; 
+			setTimeout(function() {
+				document.getElementById("travel").style.left="0px";
+				document.getElementById("travel").style.zIndex="4"; 
+				document.getElementById("yoga").style.left="-350px"; 
+				document.getElementById("yoga").style.zIndex="3"; 
+				document.getElementById("language").style.left="350px"; 
+				document.getElementById("language").style.zIndex="2"; 
+				document.getElementById("work").style.left="700px"; 
+				document.getElementById("work").style.zIndex="1"; 
 				}, 500);
 				setTimeout(function() {
 				document.getElementById("yoga").style.opacity="1"; 
-			}, 1000);
+				}, 1000);
 		
-		bannerStatus = 3;
+				bannerStatus = 3;
 			}
-			else if (bannerStatus === 3) {
-				document.getElementById("language").style.opacity="0"; 
+		else if (bannerStatus === 3) {
+			document.getElementById("language").style.opacity="0"; 
 				setTimeout(function() {
-					document.getElementById("yoga").style.left="0px";
-					document.getElementById("yoga").style.zIndex="4"; 
-					document.getElementById("language").style.left="-350px"; 
-					document.getElementById("language").style.zIndex="3"; 
-					document.getElementById("work").style.left="350px"; 
-					document.getElementById("work").style.zIndex="2"; 
-					document.getElementById("travel").style.left="700px"; 
-					document.getElementById("travel").style.zIndex="1"; 
+				document.getElementById("yoga").style.left="0px";
+				document.getElementById("yoga").style.zIndex="4"; 
+				document.getElementById("language").style.left="-350px"; 
+				document.getElementById("language").style.zIndex="3"; 
+				document.getElementById("work").style.left="350px"; 
+				document.getElementById("work").style.zIndex="2"; 
+				document.getElementById("travel").style.left="700px"; 
+				document.getElementById("travel").style.zIndex="1"; 
 				}, 500);
 				setTimeout(function() {
 				document.getElementById("language").style.opacity="1"; 
@@ -161,22 +159,22 @@ function bannerLoop() {
 		
 				bannerStatus = 4;
 			}
-			else if (bannerStatus === 4) {
-				document.getElementById("work").style.opacity="0"; 
-				setTimeout(function() {
-					document.getElementById("language").style.left="0px";
-					document.getElementById("language").style.zIndex="4"; 
-					document.getElementById("work").style.left="-350px"; 
-					document.getElementById("work").style.zIndex="3"; 
-					document.getElementById("travel").style.left="350px"; 
-					document.getElementById("travel").style.zIndex="2"; 
-					document.getElementById("yoga").style.left="700px"; 
-					document.getElementById("yoga").style.zIndex="1"; 
+		else if (bannerStatus === 4) {
+			document.getElementById("work").style.opacity="0"; 
+			setTimeout(function() {
+				document.getElementById("language").style.left="0px";
+				document.getElementById("language").style.zIndex="4"; 
+				document.getElementById("work").style.left="-350px"; 
+				document.getElementById("work").style.zIndex="3"; 
+				document.getElementById("travel").style.left="350px"; 
+				document.getElementById("travel").style.zIndex="2"; 
+				document.getElementById("yoga").style.left="700px"; 
+				document.getElementById("yoga").style.zIndex="1"; 
 				}, 500);
 				setTimeout(function() {
 				document.getElementById("work").style.opacity="1"; 
 				}, 1000);
 				bannerStatus = 1;
 			}
-		} 
+} 
 	
